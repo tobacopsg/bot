5404015f6c6ca95fd347fc0880a28d57528b9b2f
tobacopsg <vuhaihoang.yeuem@gmail.com>
2026-02-07 23:48:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
-BOT_TOKEN = os.getenv("8241969129:AAHOWb1HP11WUx53kBuuXhd9frvgTYJRC-c")
-ADMIN_ID = int(os.getenv("6050668835"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
